ff543a26fc8486016cff5b67cea3929e09375298
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-14 20:39:52 +0100

    refactor(input): Redefined Source type to be more compatible with Go-Github

--- internal/input/input.go
+++ internal/input/input.go
@@ -11,14 +11,14 @@ import (
 )
 
 type Source struct {
-	Repository string
-	CommitSha  string
+	Owner     string
+	Name      string
+	CommitSha string
 }
 
 var logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 func GetHTTPResponseBody(url string) (string, error) {
-	// Send a GET request
 	response, err := http.Get(url)
 	if err != nil {
 		logger.Error("Error fetching the URL", slog.String("error", err.Error()))
@@ -26,7 +26,6 @@ func GetHTTPResponseBody(url string) (string, error) {
 	}
 	defer response.Body.Close()
 
-	// Read the response body
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		logger.Error("Eror reading the response body", slog.String("error", err.Error()))
@@ -37,27 +36,23 @@ func GetHTTPResponseBody(url string) (string, error) {
 }
 
 func ParseRepositorySources(body string) []Source {
-	// Define the regex pattern
-	pattern := `^https://github\.com/[a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+\.git\s+[0-9a-f]{40}$`
+	pattern := `^https://github\.com/([a-zA-Z0-9_.-]+)/([a-zA-Z0-9_.-]+)\.git\s+([0-9a-f]{40})$`
 	re := regexp.MustCompile(pattern)
 
-	// Create a slice to hold the source structs
 	var sources []Source
 
 	// Scan the body line by line
 	scanner := bufio.NewScanner(strings.NewReader(body))
 	for scanner.Scan() {
 		line := scanner.Text()
-		if re.MatchString(line) {
-			// Split the line into repository and commit SHA
-			parts := strings.Fields(line)
-			if len(parts) == 2 {
-				s := Source{
-					Repository: parts[0],
-					CommitSha:  parts[1],
-				}
-				sources = append(sources, s)
+		matches := re.FindStringSubmatch(line)
+		if len(matches) == 4 {
+			s := Source{
+				Owner:     matches[1],
+				Name:      matches[2],
+				CommitSha: matches[3],
 			}
+			sources = append(sources, s)
 		}
 	}
 
