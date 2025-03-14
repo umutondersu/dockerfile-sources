0f9536b61509969be876e9d91bd324a448230538
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-14 20:40:51 +0100

    test(input): Added more tests for source parsing

--- tests/input_test.go
+++ tests/input_test.go
@@ -15,14 +15,14 @@ func TestParseRepositorySources(t *testing.T) {
 		{
 			body: "https://github.com/app-sre/qontract-reconcile.git 30af65af14a2dce962df923446afff24dd8f123e",
 			want: []input.Source{
-				{Repository: "https://github.com/app-sre/qontract-reconcile.git", CommitSha: "30af65af14a2dce962df923446afff24dd8f123e"},
+				{Owner: "app-sre", Name: "qontract-reconcile", CommitSha: "30af65af14a2dce962df923446afff24dd8f123e"},
 			},
 		},
 		{
 			body: "https://github.com/app-sre/qontract-reconcile.git 30af65af14a2dce962df923446afff24dd8f123e\nhttps://github.com/app-sre/container-images.git c260deaf135fc0efaab365ea234a5b86b3ead404",
 			want: []input.Source{
-				{Repository: "https://github.com/app-sre/qontract-reconcile.git", CommitSha: "30af65af14a2dce962df923446afff24dd8f123e"},
-				{Repository: "https://github.com/app-sre/container-images.git", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
+				{Owner: "app-sre", Name: "qontract-reconcile", CommitSha: "30af65af14a2dce962df923446afff24dd8f123e"},
+				{Owner: "app-sre", Name: "container-images", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
 			},
 		},
 		{
@@ -33,15 +33,50 @@ func TestParseRepositorySources(t *testing.T) {
 			// typo in the link
 			body: "https://gitub.com/app-sre/qontract-reconcile.git 30af65af14a2dce962df923446afff24dd8f123e\nhttps://github.com/app-sre/container-images.git c260deaf135fc0efaab365ea234a5b86b3ead404",
 			want: []input.Source{
-				{Repository: "https://github.com/app-sre/container-images.git", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
+				{Owner: "app-sre", Name: "container-images", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
 			},
 		},
 		{
 			// Wrong CommitSha format
 			body: "https://github.com/app-sre/qontract-reconcile.git 30af65af14a2dce962df923446afff24dd8f123\nhttps://github.com/app-sre/container-images.git c260deaf135fc0efaab365ea234a5b86b3ead404",
 			want: []input.Source{
-				{Repository: "https://github.com/app-sre/container-images.git", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
+				{Owner: "app-sre", Name: "container-images", CommitSha: "c260deaf135fc0efaab365ea234a5b86b3ead404"},
 			},
+		},
+		{
+			// URL without .git suffix
+			body: "https://github.com/kubernetes/kubernetes.git 8f0b92c0512afb25c8b2667ddfd1c7d5409903d3",
+			want: []input.Source{
+				{Owner: "kubernetes", Name: "kubernetes", CommitSha: "8f0b92c0512afb25c8b2667ddfd1c7d5409903d3"},
+			},
+		},
+		{
+			// Multiple invalid entries
+			body: "https://gitlab.com/user/repo.git abc123\nhttps://bitbucket.org/user/repo def456",
+			want: []input.Source{},
+		},
+		{
+			// Extra spaces
+			body: "  https://github.com/test/repo.git   abcdef1234567890abcdef1234567890abcdef12",
+			want: []input.Source{},
+		},
+		{
+			// Mixed valid and invalid formats
+			body: "https://github.com/valid/repo.git 1234567890123456789012345678901234567890\nmalformed/input\nhttps://github.com/another/valid.git 2234567890123456789012345678901234567890",
+			want: []input.Source{
+				{Owner: "valid", Name: "repo", CommitSha: "1234567890123456789012345678901234567890"},
+				{Owner: "another", Name: "valid", CommitSha: "2234567890123456789012345678901234567890"},
+			},
+		},
+		{
+			// Invalid commit SHA lengths
+			body: "https://github.com/test/short.git abc123\nhttps://github.com/test/long.git 1234567890123456789012345678901234567890extra",
+			want: []input.Source{},
+		},
+		{
+			// Case sensitivity in URL
+			body: "HTTPS://GITHUB.COM/org/repo.git abcdef1234567890abcdef1234567890abcdef12",
+			want: []input.Source{},
 		},
 	}
 	for _, tt := range tests {
