e569c74a0f75caa7cc93e2b68925031437d3462d
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-16 18:03:45 +0100

    test(jsonoutput): Added 1 more test case

--- tests/jsonoutput/jsonoutput_test.go
+++ tests/jsonoutput/jsonoutput_test.go
@@ -18,7 +18,7 @@ func TestConvert(t *testing.T) {
 		expected map[string]map[string][]string
 	}{
 		{
-			name:  "Single Dockerfile",
+			name:  "Single Dockerfile, Single Repo",
 			input: dockerFiles[0:1],
 			expected: map[string]map[string][]string{
 				"https://github.com/app-sre/qontract-reconcile.git:30af65af14a2dce962df923446afff24dd8f123e": {
@@ -27,9 +27,29 @@ func TestConvert(t *testing.T) {
 			},
 		},
 		{
-			name:  "Multiple Dockerfiles",
+			name:  "Multiple Dockerfiles, Single Repo",
 			input: dockerFiles[1:3],
 			expected: map[string]map[string][]string{
+				"https://github.com/app-sre/container-images.git:c260deaf135fc0efaab365ea234a5b86b3ead404": {
+					"jiralert/Dockerfile": {
+						"registry.access.redhat.com/ubi8/go-toolset:latest",
+						"registry.access.redhat.com/ubi8-minimal:8.2",
+					},
+					"qontract-reconcile-base/Dockerfile": {
+						"registry.access.redhat.com/ubi8/ubi:8.2",
+						"registry.access.redhat.com/ubi8/ubi:8.2",
+						"registry.access.redhat.com/ubi8/ubi:8.2",
+					},
+				},
+			},
+		},
+		{
+			name:  "Multiple Dockerfiles, Multiple Repos",
+			input: dockerFiles[0:3],
+			expected: map[string]map[string][]string{
+				"https://github.com/app-sre/qontract-reconcile.git:30af65af14a2dce962df923446afff24dd8f123e": {
+					"dockerfiles/Dockerfile": {"quay.io/app-sre/qontract-reconcile-base:0.2.1"},
+				},
 				"https://github.com/app-sre/container-images.git:c260deaf135fc0efaab365ea234a5b86b3ead404": {
 					"jiralert/Dockerfile": {
 						"registry.access.redhat.com/ubi8/go-toolset:latest",
