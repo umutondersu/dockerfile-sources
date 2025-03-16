f5abe5162c4ad205c64e92ad83db5edb8880d038
Umut Sahin Onder <umutsonder@gmail.com>
2025-03-16 17:56:54 +0100

    test(jsonoutput): Refactored the test to use testdata.DockerFiles

--- tests/jsonoutput/jsonoutput_test.go
+++ tests/jsonoutput/jsonoutput_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestConvert(t *testing.T) {
-	sources := testdata.TestSources
+	dockerFiles := testdata.TestDockerFiles
 
 	tests := []struct {
 		name     string
@@ -18,14 +18,8 @@ func TestConvert(t *testing.T) {
 		expected map[string]map[string][]string
 	}{
 		{
-			name: "Single Dockerfile, Single repo",
-			input: []ghdocker.DockerFile{
-				{
-					Source: &sources[0],
-					Path:   "dockerfiles/Dockerfile",
-					Images: []string{"quay.io/app-sre/qontract-reconcile-base:0.2.1"},
-				},
-			},
+			name:  "Single Dockerfile",
+			input: dockerFiles[0:1],
 			expected: map[string]map[string][]string{
 				"https://github.com/app-sre/qontract-reconcile.git:30af65af14a2dce962df923446afff24dd8f123e": {
 					"dockerfiles/Dockerfile": {"quay.io/app-sre/qontract-reconcile-base:0.2.1"},
@@ -33,23 +27,8 @@ func TestConvert(t *testing.T) {
 			},
 		},
 		{
-			name: "Multiple dockerfiles, Single repo",
-			input: []ghdocker.DockerFile{
-				{
-					Source: &sources[1],
-					Path:   "jiralert/Dockerfile",
-					Images: []string{"registry.access.redhat.com/ubi8/go-toolset:latest", "registry.access.redhat.com/ubi8-minimal:8.2"},
-				},
-				{
-					Source: &sources[1],
-					Path:   "qontract-reconcile-base/Dockerfile",
-					Images: []string{
-						"registry.access.redhat.com/ubi8/ubi:8.2",
-						"registry.access.redhat.com/ubi8/ubi:8.2",
-						"registry.access.redhat.com/ubi8/ubi:8.2",
-					},
-				},
-			},
+			name:  "Multiple Dockerfiles",
+			input: dockerFiles[1:3],
 			expected: map[string]map[string][]string{
 				"https://github.com/app-sre/container-images.git:c260deaf135fc0efaab365ea234a5b86b3ead404": {
 					"jiralert/Dockerfile": {
